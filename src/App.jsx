3735a77b3028d64ece9b3d8a6d724cdc08a90d0f
Ignacio Aliste <aliste11toledo@gmail.com>
2023-10-02 14:55:57 -0300

    see whose turn it is

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
+import { useState } from "react";
+
 const TUNRS = {
   X: "❌",
   O: "⚪",
 };
-const board = Array(9).fill(null);
-const Square = ({ children, updateBoard, index }) => {
-  return <div className="square">{children}</div>;
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+  return <div className={className}>{children}</div>;
 };
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  // ver visualmente quien tiene el turno
+  const [turn, setTurn] = useState(TUNRS.X);
+
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
-
       <section className="game">
         {board.map((_, index) => {
-          return (
-            <Square key={index} index={index}>
-              {index}
-            </Square>
-          );
+          return <Square key={index} index={index}></Square>;
         })}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TUNRS.X}>{TUNRS.X}</Square>
+        <Square isSelected={turn === TUNRS.O}>{TUNRS.O}</Square>
       </section>
     </main>
   );
