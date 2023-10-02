593e8e2f51ca755adbb0fdeb5356852a7993d1bd
Ignacio Aliste <aliste11toledo@gmail.com>
2023-10-02 15:08:06 -0300

    showing the move on the board

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,44 @@ const TUNRS = {
 
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? "is-selected" : ""}`;
-  return <div className={className}>{children}</div>;
+  const handleClick = () => {
+    updateBoard(index);
+  };
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
 };
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
-
   // ver visualmente quien tiene el turno
   const [turn, setTurn] = useState(TUNRS.X);
+
+  const updateBoard = (index) => {
+    // no se actualiza la posicion
+    // si ya tiene algo
+    if (board[index]) return;
+    // actualizar el tablero
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    // cambiar el turno
+    const newTurn = turn === TUNRS.X ? TUNRS.O : TUNRS.X;
+    setTurn(newTurn);
+  };
 
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
       <section className="game">
         {board.map((_, index) => {
-          return <Square key={index} index={index}></Square>;
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          );
         })}
       </section>
 
