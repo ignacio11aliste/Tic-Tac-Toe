c21f4726e699cbe33339fe3494eec539782b3cce
Ignacio Aliste <aliste11toledo@gmail.com>
2023-10-02 15:39:54 -0300

    winner detection

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,48 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
     </div>
   );
 };
+// combinaciones ganadoras
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   // ver visualmente quien tiene el turno
   const [turn, setTurn] = useState(TUNRS.X);
 
+  // ver el ganador y parar el juego
+  // null es que no ahi ganador , false es que hay un empate
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    // revisamos todas las combinaciones ganadoras
+    // para ver si X u O ganó
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    // si no hay ganador
+    return null;
+  };
+
   const updateBoard = (index) => {
     // no se actualiza la posicion
     // si ya tiene algo
-    if (board[index]) return;
+    if (board[index] || winner) return;
     // actualizar el tablero
     const newBoard = [...board];
     newBoard[index] = turn;
@@ -33,6 +65,11 @@ function App() {
     // cambiar el turno
     const newTurn = turn === TUNRS.X ? TUNRS.O : TUNRS.X;
     setTurn(newTurn);
+    // revisar si ahi un ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
   };
 
   return (
